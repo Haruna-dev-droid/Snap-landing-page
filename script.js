5ec746f724c23a996877e217f0ce91bb0bb858d5
John Doe <samailah40@gmail.com>
2025-05-22 16:38:02 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("active");
+});
