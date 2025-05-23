210def4a37eb7a551072ec09d80f0ff2488a797e
John Doe <samailah40@gmail.com>
2025-05-23 04:35:18 +0100

    added js

--- script.js
+++ script.js
@@ -2,8 +2,20 @@
 
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
+const featureBtn = document.querySelector(".features");
+const companyBtn = document.querySelector(".company");
+const featureToggle = document.querySelector(".featureToggle");
+const companyToggle = document.querySelector(".companyToggle");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   menu.classList.toggle("active");
+});
+
+featureBtn.addEventListener("click", () => {
+  featureToggle.classList.toggle("active");
+});
+
+companyBtn.addEventListener("click", () => {
+  companyToggle.classList.toggle("active");
 });
